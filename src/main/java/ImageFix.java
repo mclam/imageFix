166c3c0287b9848069459cac00cda5581a6545a6
mclam <mclam07@gmail.com>
2022-09-19 23:16:45 +0800

    Support folder scan and images decomposed in different no. of parts

--- src/main/java/ImageFix.java
+++ src/main/java/ImageFix.java
@@ -1,54 +1,59 @@
-import javax.imageio.IIOImage;
 import javax.imageio.ImageIO;
-import javax.imageio.ImageWriter;
 import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.File;
-import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.OutputStream;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collections;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 public class ImageFix {
-    public static void main(String[] args) throws IOException {
-        BufferedImage srcImage = ImageIO.read(new File("/Users/mclam/git/imageFix/src/main/resources/00023.jpeg"));
 
-        int width = srcImage.getWidth();
-        int height = srcImage.getHeight();
+    static void processPath(File path) throws IOException {
+        if (path.isDirectory()) {
+            File[] files = path.listFiles();
+            if (files != null && files.length > 0) {
+                for (File file : files) {
+                    processPath(file);
+                }
+            }
+        } else {
+            BufferedImage srcImage = ImageIO.read(path);
 
-        System.out.println(width + "x" + height);
+            int width = srcImage.getWidth();
+            int height = srcImage.getHeight();
 
-        var parts = IntStream.range(0, 20).boxed().collect(Collectors.toList());//Arrays.asList(2, 1, 3);
-        Collections.reverse(parts);
-        System.out.println("parts: " + parts);
+            System.out.println(width + "x" + height);
 
-        // decompose source image equally
-        var images = new ArrayList<BufferedImage>();
-        for (var i = 0; i < parts.size(); i++) {
-            BufferedImage subImage = srcImage.getSubimage(0, height * i / parts.size(), width, height / parts.size());
+            var parts = IntStream.range(0, Integer.parseInt(path.getParentFile().getName())).boxed().collect(Collectors.toList());//Arrays.asList(2, 1, 3);
+            Collections.reverse(parts);
+//            System.out.println("parts: " + parts);
+
+            // decompose source image equally
+            var images = new ArrayList<BufferedImage>();
+            for (var i = 0; i < parts.size(); i++) {
+                BufferedImage subImage = srcImage.getSubimage(0, height * i / parts.size(), width, height / parts.size());
 //            ImageIO.write(subImage, "jpg", new File("00023-" + i + ".jpg"));
-            images.add(subImage);
+                images.add(subImage);
+            }
+
+            // combine decomposed images by order in parts
+            BufferedImage combined = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
+            Graphics graphics = combined.getGraphics();
+            for(var i = 0; i < parts.size(); i++) {
+                graphics.drawImage(images.get(parts.get(i)), 0, height * i / parts.size(), null);
+            }
+            graphics.dispose();
+
+            // output combined image
+            File outputFile = new File(path.getPath().replaceAll("\\.jpeg", ".jpg"));
+            ImageIO.write(combined, "jpg", outputFile);
         }
+    }
 
-        // combine decomposed images by order in parts
-        BufferedImage combined = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
-        Graphics graphics = combined.getGraphics();
-        for(var i = 0; i < parts.size(); i++) {
-            graphics.drawImage(images.get(parts.get(i)), 0, height * i / parts.size(), null);
-        }
-        graphics.dispose();
-
-        // output combined image
-        File outputFile = new File("00023.jpg");
-        ImageIO.write(combined, "jpg", outputFile);
-
-//        OutputStream fileOutputStream = new FileOutputStream(outputFile);
-//        ImageWriter imageWriter = ImageIO.getImageWritersByFormatName("jpeg").next();
-//        imageWriter.setOutput(ImageIO.createImageOutputStream(fileOutputStream));
-//        imageWriter.write(null, new IIOImage(combined, null, null), null);
+    public static void main(String[] args) throws IOException {
+        String dir = args.length > 0 ? args[0] : "/Users/mclam/git/imageFix/src/main/resources/00023.jpeg";
+        processPath(new File(dir));
     }
 }
