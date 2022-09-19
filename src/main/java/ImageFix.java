729366a82665c82f61bf6b2f334fdbaba28ddd11
mclam <mclam07@gmail.com>
2022-09-19 22:47:28 +0800

    Fix sample image successfully

--- src/main/java/ImageFix.java
+++ src/main/java/ImageFix.java
@@ -1,10 +1,17 @@
+import javax.imageio.IIOImage;
 import javax.imageio.ImageIO;
+import javax.imageio.ImageWriter;
 import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.OutputStream;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class ImageFix {
     public static void main(String[] args) throws IOException {
@@ -15,18 +22,33 @@ public class ImageFix {
 
         System.out.println(width + "x" + height);
 
-        var parts = Arrays.asList(2, 1, 3);
+        var parts = IntStream.range(0, 20).boxed().collect(Collectors.toList());//Arrays.asList(2, 1, 3);
+        Collections.reverse(parts);
+        System.out.println("parts: " + parts);
+
+        // decompose source image equally
         var images = new ArrayList<BufferedImage>();
         for (var i = 0; i < parts.size(); i++) {
-            images.add(srcImage.getSubimage(0, height * i / parts.size(), width, height / parts.size()));
+            BufferedImage subImage = srcImage.getSubimage(0, height * i / parts.size(), width, height / parts.size());
+//            ImageIO.write(subImage, "jpg", new File("00023-" + i + ".jpg"));
+            images.add(subImage);
         }
-        BufferedImage combined = new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB);
+
+        // combine decomposed images by order in parts
+        BufferedImage combined = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
         Graphics graphics = combined.getGraphics();
         for(var i = 0; i < parts.size(); i++) {
-            graphics.drawImage(images.get(parts.get(i) - 1), 0, height * i / parts.size(), null);
+            graphics.drawImage(images.get(parts.get(i)), 0, height * i / parts.size(), null);
         }
         graphics.dispose();
 
-        ImageIO.write(combined, "jpg", new File("/Users/mclam/git/imageFix/src/main/resources/00023.jpg"));
+        // output combined image
+        File outputFile = new File("00023.jpg");
+        ImageIO.write(combined, "jpg", outputFile);
+
+//        OutputStream fileOutputStream = new FileOutputStream(outputFile);
+//        ImageWriter imageWriter = ImageIO.getImageWritersByFormatName("jpeg").next();
+//        imageWriter.setOutput(ImageIO.createImageOutputStream(fileOutputStream));
+//        imageWriter.write(null, new IIOImage(combined, null, null), null);
     }
 }
